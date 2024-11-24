f60b95d0a54e450da3f12fa75956da9c5ba9cab4
malinux <avelez343@gmail.com>
2024-11-23 22:19:25 -0500

    added functionality to read and add commands

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+// Provides the necessary discord.js classes
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+// Provides configuration variables
 const { token, test, server } = require('./config.json');
+// Provides utilities to interact with the file system
+const fs = require('node:fs');
+// Provides utilities to work with file and dir paths
+const path = require('node:path');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// Store and retrieve commands through a Collection, an extended Js map object
+client.commands = new Collection();
+
+// set the path for our commands and read the folders inside
+const foldersPath = path.join(__dirName, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// for each folder in commands, build the folder's absolute path, and then get all the file names inside
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file =>  file.endsWith('.js'));
+    // for each file in the folder properties, validate it a bit, and add it to client.commands
+    for (const file of commandFiles){
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // check file has data and execute properties, then add it with key-value pair <command_name, exported_module>
+        if( 'data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else{
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
